8ce497233272607ed751e97abbecf22cf0feafeb
Abdelrahman Moussa <the_eror001@hotmail.com>
2022-06-25 23:38:20 +0200

    add service to check available seats for trip

---
+++ app/Services/SeatAvailabilityService.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Bus;
+use App\Models\Trip;
+use App\Models\Station;
+use App\Models\Reservation;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class SeatAvailabilityService
+{
+  public function getAvailableSeats($request)
+  {
+    // Get the Ids of cities/stations from user input
+    $startStationId = $this->getStation($request->start_station);
+    if (!is_numeric($startStationId))
+      return $startStationId;
+    $endStationId = $this->getStation($request->end_station);
+    if (!is_numeric($endStationId))
+      return $endStationId;
+
+    // Get the trip if exists
+    try {
+      $trip = Trip::where('start_station_id', $startStationId)
+        ->where('end_station_id', $endStationId)
+        ->firstOrFail();
+    } catch (ModelNotFoundException $exception) {
+      return response()->json([
+        'message' => 'Trip not found'
+      ], 404);
+    }
+
+    /**
+     * Get available seat ids
+     */
+    $busId = $trip->bus_id;
+    // All trips with same bus and starting station
+    $commonTrips = Trip::select('id')
+      ->where('start_station_id', $startStationId)
+      ->where('bus_id', $busId)
+      ->get();
+
+    // Get all booked seats
+    $bookedSeatIdArray = Reservation::select('seat_id')
+      ->whereIn('trip_id', $commonTrips)
+      ->get()->pluck('seat_id')->toArray();
+
+    // Create bus seats array
+    $seatsCount = Bus::select('seat_count')
+      ->where('id', $busId)
+      ->first()->seat_count;
+    for ($i = 1; $i <= $seatsCount; $i++) {
+      $allBusSeatIds[$i - 1] = $i;
+    };
+
+    // Compare booked vs all bus seats and get empty seat ids
+    $availableSeatIds = array_diff($allBusSeatIds, $bookedSeatIdArray);
+    foreach ($availableSeatIds as $key => $value) {
+      $availableSeatIdsArray[] = $value;
+    }
+
+    return response()->json([
+      'trip_id'            => $trip->id,
+      'available_seats' => $availableSeatIdsArray
+    ], 200);
+  }
+
+  private function getStation($stationName)
+  {
+    try {
+      $stationId = Station::where('name', $stationName)->firstOrFail()->id;
+      return $stationId;
+    } catch (ModelNotFoundException $exception) {
+      return response()->json([
+        'message' => 'City not found. ' . $stationName
+      ], 404);
+    }
+  }
+}
