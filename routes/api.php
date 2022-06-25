e239309903f52fed4cd259017a6be1ec5c136bf2
Abdelrahman Moussa <the_eror001@hotmail.com>
2022-06-25 23:41:19 +0200

    add all routes used - lazy commit

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\Station;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SeatController;
+use App\Http\Controllers\StationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/available-seats', [Controller::class, 'availableSeats']);
+Route::post('/stations', [StationController::class, 'store']);
+Route::get('/stations', [StationController::class, 'index']);
+
+// Protected Routes
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/book-trip', [Controller::class, 'bookSeat']);
 });
